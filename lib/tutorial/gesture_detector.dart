ee86cddae76b1a892d3eb0f3219d8b0843774012
Adi Munawar <munawar.adi31@gmail.com>
2022-03-17 20:11:06 +0700

    onPanUpdate

--- lib/tutorial/gesture_detector.dart
+++ lib/tutorial/gesture_detector.dart
@@ -8,38 +8,63 @@ class GestureDetect extends StatefulWidget {
 }
 
 class _GestureDetectState extends State<GestureDetect> {
-  final double boxSize = 150.0;
   int numTap = 0;
   int numDoubleTaps = 0;
   int numLongPress = 0;
 
+  final double boxSize = 150.0;
+  double posX = 0.0;
+  double posY = 0.0;
+
   @override
   Widget build(BuildContext context) {
+    if (posX == 0) {
+      center(context);
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Gesture Detector'),
+        title: Text('Gesture Detectors'),
       ),
       body: Stack(children: [
-        GestureDetector(
-          onTap: () {
-            setState(() {
-              numTap++;
-            });
-          },
-          onDoubleTap: () {
-            setState(() {
-              numDoubleTaps++;
-            });
-          },
-          onLongPress: () {
-            setState(() {
-              numLongPress++;
-            });
-          },
-          child: Container(
-            width: boxSize,
-            height: boxSize,
-            decoration: BoxDecoration(color: Colors.red),
+        Positioned(
+          top: posY,
+          left: posX,
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                numTap++;
+              });
+            },
+            onDoubleTap: () {
+              setState(() {
+                numDoubleTaps++;
+              });
+            },
+            onLongPress: () {
+              setState(() {
+                numLongPress++;
+              });
+            },
+            onPanUpdate: (DragUpdateDetails details) {
+              setState(() {
+                double deltaX = details.delta.dx;
+                double deltaY = details.delta.dy;
+                posX += deltaX;
+                posY += deltaY;
+              });
+            },
+            onVerticalDragUpdate: (DragUpdateDetails details) {
+              setState(() {
+                double delta = details.delta.dy;
+                posY += delta;
+              });
+            },
+            child: Container(
+              width: boxSize,
+              height: boxSize,
+              decoration: BoxDecoration(color: Colors.red),
+            ),
           ),
         ),
       ]),
@@ -52,5 +77,15 @@ class _GestureDetectState extends State<GestureDetect> {
         ),
       ),
     );
+  }
+
+  void center(BuildContext context) {
+    posX = (MediaQuery.of(context).size.width / 2) - boxSize / 2;
+    posY = (MediaQuery.of(context).size.height / 2) - boxSize / 2 - 30;
+
+    setState(() {
+      posX = posX;
+      posY = posY;
+    });
   }
 }
